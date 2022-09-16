dcf8e3ee47875d956d7e2606a4c63e6fc804ba9c
shabashaash <43162766+shabashaash@users.noreply.github.com>
2022-09-16 22:19:40 +0300

    Update __init__.py

--- clmr/datasets/__init__.py
+++ clmr/datasets/__init__.py
@@ -5,9 +5,11 @@ from .librispeech import LIBRISPEECH
 from .gtzan import GTZAN
 from .magnatagatune import MAGNATAGATUNE
 from .million_song_dataset import MillionSongDataset
+from .playlists import PLAYLISTS
+from .recomendations import RECOMENDATIONS
 
 
-def get_dataset(dataset, dataset_dir, subset, download=True):
+def get_dataset(dataset, dataset_dir, subset, download=True): #playlist_paths
 
     if not os.path.exists(dataset_dir):
         os.makedirs(dataset_dir)
@@ -22,6 +24,10 @@ def get_dataset(dataset, dataset_dir, subset, download=True):
         d = MAGNATAGATUNE(root=dataset_dir, download=download, subset=subset)
     elif dataset == "msd":
         d = MillionSongDataset(root=dataset_dir, subset=subset)
+    elif dataset == "playlists":
+        d = PLAYLISTS(root=dataset_dir, subset=subset)
+    elif dataset == "recomend":
+        d = RECOMENDATIONS(root=dataset_dir, subset=subset)
     else:
         raise NotImplementedError("Dataset not implemented")
     return d
